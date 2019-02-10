e13281da8f27a785b769a6c110942b808f7ae237
Kiran K J <kiran.jolly@cabotsolutions.com>
2019-02-10 15:33:55 +0530

    1) Replaced pricelist image with html render. 2) Minor beatification/styele chages

--- src/components/OperatorsPriceList/OperatorsPriceList.js
+++ src/components/OperatorsPriceList/OperatorsPriceList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './OperatorsPriceList.css';
-import image from '../../assets/operator-price-list.png'; 
+import jsonData from '../../assets/operators.json'; 
 
 class OperatorsPriceList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            operators: jsonData
+        }
+    }
     render() {
         return (
             <div className="price-list-container">
-                <img className="price-list-image" src={image} alt="Price List"></img>
+                <h2>Operator Costs</h2><br/>
+                {
+                    Object.keys(this.state.operators).map((item) => {
+                        return (<div key={item}>
+                            <h3>{item}:</h3><table border="1"><thead><tr><th>Number Prefix</th><th>Cost</th></tr></thead><tbody>
+                                {this.state.operators[item].map((item) => {
+                                    return (
+                                        <tr key={item.prefix}><td>{item.prefix}</td><td>{item.cost}</td></tr>
+                                    );
+                                })}
+                                </tbody></table><br/></div>);
+                    })
+                }
             </div>
         );
     }
